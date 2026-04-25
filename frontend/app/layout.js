3b83a49b23d9a4314d30a12e71b488e2d3498d40
Shivathej Gowda <tejj69@gmail.com>
2026-04-25 11:15:12 +0530

    fix vercel root directory

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,8 +2,8 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark"> {/* FORCE DARK FOR NOW */}
-      <body className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
+    <html lang="en">
+      <body className="bg-gray-100 text-gray-900">
         {children}
       </body>
     </html>
